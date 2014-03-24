2fb921ee5520619461f45180d59639d534ac57a4
Lars Schmertmann <lars@tzi.de>
2014-03-24 11:58:05 +0100

    Coap blockwise update to draft 14

--- device-coap/resource.c
+++ device-coap/resource.c
@@ -13,41 +13,37 @@ static size_t big_msg_len = 0;
 static coap_separate_t request_metadata[1];
 
 void resource_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {
-  if (coap_block1_handler(request, response, big_msg, &big_msg_len, 256)) {
+  if (*offset == 0) {
+    if (coap_block1_handler(request, response, big_msg, &big_msg_len, 256)) {
       return;
-  }
+    }
+    printf("MSG: %s\n", big_msg);
 
-  printf("MSG: %s\n", big_msg);
+    coap_separate_accept(request, request_metadata);
+    erbium_status_code = NO_ERROR;
+    request_metadata->block2_size = 32;
 
-  coap_separate_accept(request, request_metadata);
-  sendAnswer(NULL, request);
-}
-
-void sendAnswer(void *data, void* resp) {
-  request_metadata->block2_size = 32;
-
-  uint8_t buffer[32];
-  memcpy(buffer, big_msg + (request_metadata->block2_num * 32), 32);
-  uint32_t read = 32;
-  if ((request_metadata->block2_num + 1) * 32 >= big_msg_len) {
-    read = big_msg_len % 32;
-    if (read == 0) read = 32;
-  } else {
-    read = 0;
-  }
-
-  coap_transaction_t *transaction = NULL;
-  if ( (transaction = coap_new_transaction(request_metadata->mid, &request_metadata->addr, request_metadata->port)) ) {
-    coap_packet_t response[1];
     coap_separate_resume(response, request_metadata, CREATED_2_01);
     coap_set_header_content_format(response, TEXT_PLAIN);
-    coap_set_payload(response, buffer, read == 0 ? request_metadata->block2_size : read);
-    coap_set_header_block2(response, request_metadata->block2_num, read == 0 ? 1 : 0, request_metadata->block2_size);
-    // TODO Warning: No check for serialization error.
-    transaction->packet_len = coap_serialize_message(response, transaction->packet);
-    transaction->callback = (read == 0 ? &sendAnswer : NULL);
-    coap_send_transaction(transaction);
-    request_metadata->block2_num++;
+    coap_set_payload(response, big_msg, big_msg_len > 32 ? 32 : big_msg_len);
+    if (big_msg_len > 32) *offset = 32;
+  } else {
+    if (*offset >= big_msg_len) {
+      coap_set_status_code(response, BAD_OPTION_4_02);
+      coap_set_payload(response, "BlockOutOfScope", 15);
+      return;
+    }
+
+    memcpy(buffer, big_msg + *offset, 32);
+    if (big_msg_len - *offset < preferred_size) {
+      preferred_size = big_msg_len - *offset;
+      *offset = -1;
+    } else {
+      *offset += preferred_size;
+    }
+
+    REST.set_header_content_type(response, TEXT_PLAIN);
+    REST.set_response_payload(response, buffer, preferred_size);
   }
 }
 
